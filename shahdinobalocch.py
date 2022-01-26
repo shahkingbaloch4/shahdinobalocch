f3c3df244842a81271d3216ae4263f72b179d36b
shahkingbaloch4 <97821743+shahkingbaloch4@users.noreply.github.com>
2022-01-26 13:46:48 +0500

    Create shahdinobalocch.py
    pkg update
    pkg upgrade
    pkg install python -y
    pkg install python2 -y
    pkg install git -y
    pip2 install requests
    pip2 install mechanize
    termux-setup-storage
    git clone https://github.com/shahkingbaloch4/shahdinobalocch.git
    cd shahdinobalocch
    python2 shahdinobalocch.py

---
+++ shahdinobalocch.py
@@ -0,0 +1,9 @@
+pkg update
+pkg upgrade
+pkg install python -y
+pkg install python2 -y
+pkg install git -y
+pip2 install requests
+pip2 install mechanize
+termux-setup-storage
+git clone https://github.com/shahkingbaloch4/shahdinobalocch.git
